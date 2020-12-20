80c81b16d4b71963d4ea4a263c8c548d82b2c611
RAKHIMOV Dzhamil <Dzhamil.RAKHIMOV@raiffeisen.ru>
2020-12-20 18:27:06 +0300

    init commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://i7.pngflow.com/pngimage/554/814/png-google-chrome-web-browser-computer-icons-firefox-logo-monochrome-google-chrome-chrome-os-clipart-thumb.png"
+                alt=""/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://bigpicture.ru/wp-content/uploads/2015/01/AUcommunes10.jpg" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
