07ec78050368cc4280fa2df9e2b3496f0725bae0
RAKHIMOV Dzhamil <Dzhamil.RAKHIMOV@raiffeisen.ru>
2021-01-24 16:07:28 +0300

    lesson 14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img
                 src="https://i7.pngflow.com/pngimage/554/814/png-google-chrome-web-browser-computer-icons-firefox-logo-monochrome-google-chrome-chrome-os-clipart-thumb.png"
                 alt=""/>
