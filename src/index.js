bf23615cdc6335b94e02bc422890e24ba3271895
RAKHIMOV Dzhamil <Dzhamil.RAKHIMOV@raiffeisen.ru>
2021-02-05 20:53:32 +0300

    lesson 37

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state, {addPost, subscribe, updateNewPostText} from "./redux/state";
 import ReactDOM from "react-dom";
 import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
+import store from "./redux/state";
 
 let rerenderEntireThree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+                <App state={state}
+                     addPost={store.addPost.bind(store)}
+                     updateNewPostText={store.updateNewPostText.bind(store)}/>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
 
-rerenderEntireThree(state);
-subscribe(rerenderEntireThree);
+
+rerenderEntireThree(store.getState());
+store.subscribe(rerenderEntireThree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,100 +1,105 @@
-let state = {
-    profilePage: {
-        posts: [
-            {id: 0, message: 'Hi, have are you?', likeCounts: 12},
-            {id: 1, message: 'It\'s my first posts', likeCounts: 988}
-        ],
-        newPostText: ''
-    },
-    messagesPage: {
-        dialogs: [
-            {
-                id: 0,
-                name: 'Dzhamil',
-                avatarUrl: 'https://maski-instagrama.ru/wp-content/uploads/a/6/c/a6c8a2ce39a7a097395e72c9e778a381.jpeg'
-            },
-            {id: 1, name: 'Dima', avatarUrl: 'https://pbs.twimg.com/profile_images/998651045491367939/ySo7CMSH.jpg'},
-            {
-                id: 2,
-                name: 'Katya',
-                avatarUrl: 'https://proprikol.ru/wp-content/uploads/2019/09/prikolnye-foto-na-avu-dlya-devochek-60.jpg'
-            },
-            {
-                id: 3,
-                name: 'Pasha',
-                avatarUrl: 'https://vraki.net/sites/default/files/mood/zh.jpg'
-            },
-            {
-                id: 4,
-                name: 'Kristina',
-                avatarUrl: 'https://w-dog.ru/wallpapers/4/0/460972246140572/devushka-pomada-krasnye-guby-shlyapa-ruka-kolca.jpg'
-            },
-            {
-                id: 5,
-                name: 'Alena',
-                avatarUrl: 'https://klike.net/uploads/posts/2018-06/1528374055_2.jpg'
-            }
-        ],
-        messages: [
-            {id: 0, src: 'producer', message: 'Hello'},
-            {
-                id: 1,
-                src: 'producer',
-                message: 'Have are you? Have are you? Have are you? Have are you? Have are you? Have are you? Have are you? Have are you?'
-            },
-            {id: 1, src: 'consumer', message: 'Fine'},
-            {id: 1, src: 'producer', message: 'What?'},
-            {id: 2, src: 'consumer', message: 'Yo'}
-        ]
-    },
-    navbar: {
-        navbarItems: [
-            {to: '/profile', itemName: 'Profile'},
-            {to: '/dialogs', itemName: 'Messages'},
-            {to: '/news', itemName: 'News'},
-            {to: '/music', itemName: "Music"},
-            {to: '/settings', itemName: "Settings"}
-        ],
-        friends: [
-            {
-                name: 'Dzhamil',
-                avatarUrl: 'https://maski-instagrama.ru/wp-content/uploads/a/6/c/a6c8a2ce39a7a097395e72c9e778a381.jpeg'
-            },
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 0, message: 'Hi, have are you?', likeCounts: 12},
+                {id: 1, message: 'It\'s my first posts', likeCounts: 988}
+            ],
+            newPostText: ''
+        },
+        messagesPage: {
+            dialogs: [
+                {
+                    id: 0,
+                    name: 'Dzhamil',
+                    avatarUrl: 'https://maski-instagrama.ru/wp-content/uploads/a/6/c/a6c8a2ce39a7a097395e72c9e778a381.jpeg'
+                },
+                {
+                    id: 1,
+                    name: 'Dima',
+                    avatarUrl: 'https://pbs.twimg.com/profile_images/998651045491367939/ySo7CMSH.jpg'
+                },
+                {
+                    id: 2,
+                    name: 'Katya',
+                    avatarUrl: 'https://proprikol.ru/wp-content/uploads/2019/09/prikolnye-foto-na-avu-dlya-devochek-60.jpg'
+                },
+                {
+                    id: 3,
+                    name: 'Pasha',
+                    avatarUrl: 'https://vraki.net/sites/default/files/mood/zh.jpg'
+                },
+                {
+                    id: 4,
+                    name: 'Kristina',
+                    avatarUrl: 'https://w-dog.ru/wallpapers/4/0/460972246140572/devushka-pomada-krasnye-guby-shlyapa-ruka-kolca.jpg'
+                },
+                {
+                    id: 5,
+                    name: 'Alena',
+                    avatarUrl: 'https://klike.net/uploads/posts/2018-06/1528374055_2.jpg'
+                }
+            ],
+            messages: [
+                {id: 0, src: 'producer', message: 'Hello'},
+                {
+                    id: 1,
+                    src: 'producer',
+                    message: 'Have are you? Have are you? Have are you? Have are you? Have are you? Have are you? Have are you? Have are you?'
+                },
+                {id: 1, src: 'consumer', message: 'Fine'},
+                {id: 1, src: 'producer', message: 'What?'},
+                {id: 2, src: 'consumer', message: 'Yo'}
+            ]
+        },
+        navbar: {
+            navbarItems: [
+                {to: '/profile', itemName: 'Profile'},
+                {to: '/dialogs', itemName: 'Messages'},
+                {to: '/news', itemName: 'News'},
+                {to: '/music', itemName: "Music"},
+                {to: '/settings', itemName: "Settings"}
+            ],
+            friends: [
+                {
+                    name: 'Dzhamil',
+                    avatarUrl: 'https://maski-instagrama.ru/wp-content/uploads/a/6/c/a6c8a2ce39a7a097395e72c9e778a381.jpeg'
+                },
 
-            {
-                name: 'Alena',
-                avatarUrl: 'https://klike.net/uploads/posts/2018-06/1528374055_2.jpg'
-            },
-            {
-                name: 'Pasha',
-                avatarUrl: 'https://vraki.net/sites/default/files/mood/zh.jpg'
-            }
-        ]
+                {
+                    name: 'Alena',
+                    avatarUrl: 'https://klike.net/uploads/posts/2018-06/1528374055_2.jpg'
+                },
+                {
+                    name: 'Pasha',
+                    avatarUrl: 'https://vraki.net/sites/default/files/mood/zh.jpg'
+                }
+            ]
+        }
+    },
+    getState() {
+        return this._state;
+    },
+    addPost() {
+        let newPost = {
+            id: 33,
+            message: this._state.profilePage.newPostText,
+            likeCounts: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this.getState().profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
+    _callSubscriber() {
+        console.log('state is changed');
     }
 }
 
-export const addPost = ()=> {
-    let newPost = {
-        id: 33,
-        message: state.profilePage.newPostText,
-        likeCounts: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireThree(state);
-}
-
-export const updateNewPostText = (newText)=> {
-    state.profilePage.newPostText = newText;
-    rerenderEntireThree(state);
-}
-
-export const subscribe = (observer) => { //паттерн publisher subscriber // observer
-    rerenderEntireThree = observer;
-}
-
-let rerenderEntireThree = () => {
-    console.log('state is changed');
-}
-
-export default state;
+export default store;
